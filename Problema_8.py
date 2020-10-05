b997d7f164d5ea843ec27589f6d0484e28503ee3
CodreanuAlexandru <70651712+CodreanuAlexandru@users.noreply.github.com>
2020-10-05 15:20:22 +0300

    Update Problema_8.py

--- Problema_8.py
+++ Problema_8.py
@@ -1,4 +1,4 @@
 a=input("Prima cifra:")
 b=input("A doua cifra:")
 c=input("A treia cifra:")
-print ("Numerele sunt:",a,b,c,"\n" ,a,c,b,"\n" ,b,a,c,"\n" ,b,c,a,"\n" ,c,a,b,)
+print ("Numerele sunt:",a,b,c,"\n" ,a,c,b,"\n" ,b,a,c,"\n" ,b,c,a,"\n" ,c,a,b, sep="")
